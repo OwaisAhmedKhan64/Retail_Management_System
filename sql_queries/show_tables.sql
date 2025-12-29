0e6fe16c32ac8e529b49c390a7fafcd4abc1f512
Owais Ahmed Khan <owaisahmedkhan64@gmail.com>
2025-12-29 21:42:07 +0500

    Stop tracking sql_queries directory

--- sql_queries/show_tables.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM retail_management_system.customers;
-SELECT * FROM retail_management_system.products;
-SELECT * FROM retail_management_system.sales;
-SELECT * FROM retail_management_system.sale_items;
-SELECT * FROM retail_management_system.users;
-SELECT * FROM retail_management_system.restocks
--- sql_queries/x.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE products ADD COLUMN is_deleted TINYINT(1) DEFAULT 0;
